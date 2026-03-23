0f09def726be35b8bdcd1fd5bd366415de853370
Dhanush-7718 <dhanushkumr.07@gmail.com>
2026-03-23 18:36:40 +0530

    My full stack portfolio project

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "Backend is running!"
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS contacts (name TEXT, email TEXT, message TEXT)')
+    cursor.execute("INSERT INTO contacts VALUES (?, ?, ?)", (name, email, message))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Data saved!"})
+
+if __name__ == '__main__':
+    app.run()
